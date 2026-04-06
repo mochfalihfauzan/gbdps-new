6a0f663c70f0e5d39df24a90e13c5ea95f2eb0f2
unknown <falihfauzan64@gmail.com>
2026-04-06 15:25:33 +0700

    feat: implement user login with session mechanism

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, BadRequestException } from '@nestjs/common';
+import { Body, Controller, Get, Post, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -19,6 +20,18 @@ export class UsersController {
     } catch (error) {
       throw new BadRequestException({
         error: 'Email sudah terdaftar atau password invalid',
+      });
+    }
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginUserDto) {
+    try {
+      const token = await this.usersService.login(dto);
+      return { data: token };
+    } catch (error) {
+      throw new UnauthorizedException({
+        error: 'Email atau password salah',
       });
     }
   }
